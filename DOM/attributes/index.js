ffa9749171edc322d6663b8df05b5c980e66245c
TanjaI <tanjuska611@gmail.com>
2021-06-03 23:00:07 +0200

    attributes finished

--- DOM/attributes/index.js
+++ DOM/attributes/index.js
@@ -1,10 +1,11 @@
 function validation(){
    var form=document.querySelector("form");
-    form.querySelectorAll("input");
-    form.forEach(function(element){
-        if (element.hasAttribute("required")&& element.textContent===""){
-             element.className=".redBorder";
+   var list= form.querySelectorAll("input");
+   list.forEach(function(element){
+        if (element.hasAttribute("required")&& element.value===""){
+             element.classList.value="redBorder";
         }
+        
     })
 }
 
