c0478d7237543aa6e99946a94d2bd2db56d956bf
TanjaI <tanjuska611@gmail.com>
2021-06-20 23:09:21 +0200

    task 3

---
+++ JQUERY/task2/main.js
@@ -0,0 +1,32 @@
+$(function(){
+    var list=['https://picsum.photos/200','https://picsum.photos/201','https://picsum.photos/202','https://picsum.photos/203','https://picsum.photos/204','https://picsum.photos/205','https://picsum.photos/206'];
+var div=$('<div> </div>');
+ div.addClass('gallery')
+// div.text('cao');
+// console.log(div);
+
+
+
+
+
+list.forEach(function(element){
+  var img=$('<img>');
+  var el=element;
+  var size=(Math.round(Math.random()*250+1));
+  img.attr('src',el).width(size);
+  div.append(img);
+})
+
+$('body').prepend(div);
+
+$('img').each(function(index,element){
+ 
+  if($(element).width()>200){
+    return false;
+  }
+$(element).css('border','2px solid green');
+})
+
+var title=$('<h1>This is title</h1>');
+div.prepend(title);
+})
---
+++ JQUERY/task4/main.js
@@ -0,0 +1,10 @@
+var img1=$('img').first();
+img1.addClass('selected');
+
+function removeAdd(){
+    img1.removeClass('selected');
+    $('body').find('div');
+
+}
+
+removeAdd();
